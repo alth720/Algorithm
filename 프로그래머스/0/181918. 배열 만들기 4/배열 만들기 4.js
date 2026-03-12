894935441f8380420b408abb200f06b75f011510
alth720 <misokim0720@gmail.com>
2026-03-12 22:38:18 +0900

    [level 0] Title: 배열 만들기 4, Time: 30.57 ms, Memory: 39.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.js
@@ -0,0 +1,18 @@
+function solution(arr) {
+    var stk = [];
+    
+    for(let i = 0; i < arr.length; i++) {
+        if(stk.length != 0 && stk[stk.length - 1] < arr[i]) {
+            stk.push(arr[i]);
+        }
+        else if(stk.length != 0 && stk[stk.length - 1] >= arr[i]) {
+            stk.pop();
+            i -= 1; 
+        }
+        else {
+            stk.push(arr[i]);
+        }
+    }
+    
+    return stk;
+}
